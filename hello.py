3c0734edd76000e6a82bbecdadd7938a172fabe6
Crongz <leejamesws@gmail.com>
2015-10-28 12:04:43 -0500

    return json fixed

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ def user_info():
 
 @app.route('/info/users/data')
 def user_data():
-	return jsonify({ 'Name': { 'first': 'James', 'last': 'Lee', 'year': 2018, 'registered': 2015, 'Average Lap Completion Time': 20, 
+	return flask.jsonify({ 'Name': { 'first': 'James', 'last': 'Lee', 'year': 2018, 'registered': 2015, 'Average Lap Completion Time': 20, 
 		'Average Speed': 10, 'Fastest Lap Time': 10, 'Total Laps Completed': 20, 'Total Distance Ran': 10, 'Total time spent running':30  }
 	#return jsonify( Name = g.user.username,)
 	#return 'all information of a specific user'
