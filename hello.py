bc530bac5dceef3bad33770fe83f97603f3c250f
Crongz <leejamesws@gmail.com>
2015-10-28 12:21:03 -0500

    return json fixed

--- hello.py
+++ hello.py
@@ -7,19 +7,21 @@ app = Flask(__name__)
 def index():
 	return 'SIGCHI-UIUC lapcounter backend'
 
+#ran array of all users
 @app.route('/info/users')
 def user_info():
-	return 'an array of all users /user/info'
+	return jsonify({ '1': 'user1', '2':'user2', '3':'user3'})
 
+#return all information of a specific user
 @app.route('/info/users/data')
 def user_data(name):
-	#return jsonify({ 'Name': 'James Lee', 'year': 2018, 'registered': 2015, 'Average Lap Completion Time': 20, 
-	#	'Average Speed': 10, 'Fastest Lap Time': 10, 'Total Laps Completed': 20, 'Total Distance Ran': 10, 'Total time spent running':30  })
-	return jsonify( Name = name)
+	return jsonify({ 'Name': 'James Lee', 'year': 2018, 'registered': 2015, 'Average_Lap_Completion_Time': 20, 
+		'Average_Speed': 10, 'Fastest_Lap_Time': 10, 'Total_Laps_Completed': 20, 'Total_Distance_Ran': 10, 'Total_time_spent_running':30 })
 
+#return all information of a specific running session
 @app.route('/session/info')
 def session_info():
-	return 'all information of a specific running session'
+	return jsonify({ 'Average_Lap_Speed':100 ,'Fastest_Lap_Speed':20, 'Duration_Session':30, 'Start_Session':'2:20', 'End_Session':'1:10'})
 
 
 with app.test_request_context():
