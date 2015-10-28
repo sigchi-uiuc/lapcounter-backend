626a12f130b3923693a5fa292d21c12640b4e259
Crongz <leejamesws@gmail.com>
2015-10-28 11:36:34 -0500

    view.py set up

--- hello.py
+++ hello.py
@@ -4,5 +4,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
 	return 'SIGCHI-UIUC lapcounter backend'
+
+@app.route('/info/users')
+def user_info():
+	return 'an array of all users /user/info'
+
+@app.route('/info/users/data')
+def user_data():
+	return 'all information of a specific user'
+
+@app.route('/session/info')
+def session_info():
+	return 'all information of a specific running session'
+
+
