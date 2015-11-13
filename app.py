fd4ef8879f257368707d30ad0c600421fd19362c
Crongz <leejamesws@gmail.com>
2015-11-13 11:28:00 -0600

    model.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+import models
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/lab_counter_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://igsusmnkuuuiff:aJMRa9Tw81so2ncyhLvDED6prS@ec2-107-21-222-62.compute-1.amazonaws.com:5432/dflphcsocv57hu'
 db = SQLAlchemy(app)
+
 
 # Set "homepage" to index.html
 @app.route('/')
@@ -14,14 +16,13 @@ def index():
 #ran array of all users
 @app.route('/info/users')
 def user_info():
-    #return jsonify({ '1': 'user1', '2':'user2', '3':'user3'})
-    return jsonify(json_list=[i.serialize for i in db.session.query(User).all()])
-
+    foos = User.query.all()
+    results = [ foo.as_dict() for foo in foos ]
+    return jsonify({'results': results})
 
 #return all information of a specific user
 @app.route('/info/<input_users>/data')
 def user_data(input_users):
-    #return jsonify({ 'Name': 'James Lee', 'year': 2018, 'registered': 2015, 'Average_Lap_Completion_Time': 20, 'Average_Speed': 10, 'Fastest_Lap_Time': 10, 'Total_Laps_Completed': 20, 'Total_Distance_Ran': 10, 'Total_time_spent_running':30 })
     result = db.session.query(User).filter(User.name == input_users).one()
     return jsonify(result.data.serialize)
 
@@ -29,57 +30,6 @@ def user_data(input_users):
 @app.route('/session/info')
 def session_info():
     return jsonify({ 'Average_Lap_Speed':100 ,'Fastest_Lap_Speed':20, 'Duration_Session':30, 'Start_Session':'2:20', 'End_Session':'1:10'})
-
-# Create table of users on database 
-class User(db.Model):
-    __tablename__ = "users_table"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
-    registered = db.Column(db.String(20))
-    data_id = db.Column(db.Integer, db.ForeignKey('data_table.id'))
-    # define relationship
-    data = db.relationship('Data', uselist=False, backref="users_table")
-
-
-    def __init__(self, name, registered, data):
-        self.name = name
-        self.registered = registered
-        self.data = data
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
-
-    @property
-    def serialize(self):
-        return { self.id : self.name}
-
-#Create table of user's data on database
-class Data(db.Model):
-    __tablename__ = "data_table"
-    id = db.Column(db.Integer, primary_key=True)
-    avg_lap_completed_time = db.Column(db.Integer)
-    avg_speed = db.Column(db.Integer)
-    fastest_lap_time  = db.Column(db.Integer)
-    total_laps_completed = db.Column(db.Integer)
-    total_distance_ran = db.Column(db.Integer)
-    total_time_spent_running = db.Column(db.Integer)
-
-    def __init__(self, avg_lap_completed_time, avg_speed, fastest_lap_time, total_laps_completed, total_distance_ran, total_time_spent_running):
-        self.avg_lap_completed_time = avg_lap_completed_time
-        self.avg_speed = avg_speed
-        self.fastest_lap_time = fastest_lap_time
-        self.total_laps_completed = total_laps_completed
-        self.total_distance_ran = total_distance_ran
-        self.total_time_spent_running = total_time_spent_running
-
-    def __repr__(self):
-        return '<id %r>' % self.id
-
-    @property
-    def serialize(self):
-        return {'avg_lap_completed_time' : self.avg_lap_completed_time, 'avg_speed': self.avg_speed, 
-        'fastest_lap_time': self.fastest_lap_time, 'total_laps_completed': self.total_laps_completed, 
-        'total_distance_ran': self.total_distance_ran, 'total_time_spent_running': self.total_time_spent_running}
 
 # Save e-mail to database and send to success page
 @app.route('/upload', methods=['POST'])
